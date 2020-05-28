0a3d4fd612af56de0569feb5868ae1146528224c
chickenLemonade <rosekatherinecollins@icloud.com>
2020-05-28 19:45:10 +0000

    Tried to load an image for prey, but it didn't work!!!

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -8,12 +9,28 @@ screen_info = pygame.display.Info()
 # set size of game board to screen size
 screen_size = (width, height) = (screen_info.current_w, screen_info.current_h)
 
-
 # set screen to screen size
 screen = pygame.display.set_mode(screen_size)
 
 #color of background
 color = (235, 204, 52)
+
+# loading the prey image
+prey_image = pygame.image.load("prey.jpg")
+prey_image = pygame.transform.smoothscale(prey_image, (100, 100))
+
+prey_rect = prey_image.get_rect()
+
+# places image
+prey_rect.center = (width//2, height//2)
+
+#variables to move prey
+speed = pygame.math.Vector2(30, 5)
+rotation = random.randint(0, 360)
+speed.rotate_ip(rotation)
+
+pygame.transform.rotate(prey_image, 180 - rotation)
+
 
 # game code
 def main():
